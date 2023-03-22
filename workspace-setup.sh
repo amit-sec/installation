32772dd8ae5295a441f97507a81896605407389d
amit-sec <66508064+amit-sec@users.noreply.github.com>
2023-03-22 12:39:09 +0530

    Update workspace-setup.sh

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -1,8 +1,10 @@
-echo "Installing Go lang from go.dev"
+apt-get update 
+
+echo "---------Installing Go lang from go.dev-----------------"
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-echo "Extracting in /usr/local"
+echo "---------Extracting in /usr/local-----------------------"
 tar -C /usr/local/ -xzf go1.20.2.linux-amd64.tar.gz
-echo "setting up environmental variables in zsh"
+echo "---------setting up environmental variables in zsh---------"
 echo "export GOPATH=/root/go-workspace" >> ~/.zshrc
 echo "export GOROOT=/usr/local/go" >> ~/.zshrc
 echo "PATH=$PATH:$GOROOT/bin/:$GOPATH/bin" >> ~/.zshrc
